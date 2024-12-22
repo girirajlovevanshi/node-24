905571c25113a2e097b169df542a1b8144a33950
girirajlovevanshi <girirajlovewanshi@gmail.com>
2024-12-22 22:14:01 +0530

    an example to demonstrate event handling using EventEmitter.

--- q3/index.js
+++ q3/index.js
@@ -1,6 +1,5 @@
 
 //Write a Node.js script to create a web server that returns 'Welcome to Node.js DashBoard'.
-Write an example to demonstrate event handling using EventEmitter.
 
 import http from 'http'
 
