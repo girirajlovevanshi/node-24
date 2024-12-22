8ddb21970e4b8463bf9c34f197d8be6263c59bbb
girirajlovevanshi <girirajlovewanshi@gmail.com>
2024-12-22 21:53:36 +0530

    Node.js script to create a web server that returns 'Welcome to Node.js DashBoard'.

---
+++ q3/index.js
@@ -0,0 +1,18 @@
+
+//Write a Node.js script to create a web server that returns 'Welcome to Node.js DashBoard'.
+
+import http from 'http'
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{
+        "content-type":"text/plain"
+    })
+    res.end("Welcome to Node.js DashBoard")
+})
+
+const port = 8080
+
+server.listen(port,()=>{
+    console.log('server is running on Port:',port);
+    
+})
